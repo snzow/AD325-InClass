1a17d36c5276b9d620cab3befdf9c4d3ca8b2d4e
EricLloydNSC-CS <eric.lloyd@seattlecolleges.edu>
2022-11-13 22:28:52 -0500

    Week 8 Incomplete Commit

---
+++ src/main/java/Week8_Incomplete/TreePackage/TreeInterface.java
@@ -0,0 +1,10 @@
+package Week8_Incomplete.TreePackage;
+
+public interface TreeInterface<T>
+{
+    public T getRootData();
+    public int getHeight();
+    public int getNumberOfNodes();
+    public boolean isEmpty();
+    public void clear();
+} // end TreeInterface
